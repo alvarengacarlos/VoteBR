7195cfbe62def2ddbc0bb05e615354c1a33e3e57
alvarengacarlos <carlosho200@hotmail.com>
2022-04-24 10:32:37 -0400

    feat: Create updateElectionResearch, retrieveElectionResearchWithoutStarting, _getQueryResultForQueryString, _getAllResults  methods

--- contract/lib/Repository/AdminRepository.js
+++ contract/lib/Repository/AdminRepository.js
@@ -1,6 +1,11 @@
 const ExistingRecord = require("../Exceptions/ExistingRecord");
+const ResearchWithoutStartingDoesNotExist = require("../Exceptions/ResearchWithoutStartingDoesNotExist");
 
 class AdminRepository {
+
+    //TODO: Não deixar criar pesquisa eleitoral quanto já tiver uma criaca em aberto e sem iniciar.
+    //Não deixar criar pesquisa eleitoral até que tenha finalizado a atual
+    //Não deixar deixar mexer na pesquisa eleitoral quando estiver em andamento
     async createElectionResearch(ctx, electionResearch) {        
         const electionResearchExists = await this.electionResearchExists(ctx, electionResearch.getId());
         if (electionResearchExists) {
@@ -14,6 +19,52 @@ class AdminRepository {
         const buffer = await ctx.stub.getState(electionResearchId);
         return !!buffer && buffer.length > 0;
     }
+
+    async updateElectionResearch(ctx, electionResearch) {
+        await ctx.stub.putState(electionResearch.getId(), electionResearch.serializerInBuffer());
+    }
+
+    async retrieveElectionResearchWithoutStarting(ctx) {
+        let queryString = {};
+		queryString.selector = {};
+		queryString.selector.start = false;
+		queryString.selector.close = false;        
+
+        const resultsArray = await this._getQueryResultForQueryString(ctx, JSON.stringify(queryString));
+
+        if (resultsArray.length == 0) {
+            throw new ResearchWithoutStartingDoesNotExist();
+        }
+
+        return resultsArray[0];
+    }
+
+    async _getQueryResultForQueryString(ctx, queryString) {
+		let resultsIterator = await ctx.stub.getQueryResult(queryString);
+		let resultsArray = await this._getAllResults(resultsIterator);
+
+		return resultsArray;
+	}
+
+    async _getAllResults(iterator) {
+		let allResults = [];
+		let res = await iterator.next();
+		while (!res.done) {
+			if (res.value && res.value.value.toString()) {
+				let jsonRes;				
+				try {					
+                    jsonRes = JSON.parse(res.value.value.toString('utf8'));
+                    allResults.push(jsonRes);
+				} catch (err) {
+					console.log(err);
+					jsonRes = res.value.value.toString('utf8');
+				}				
+			}
+			res = await iterator.next();
+		}
+		iterator.close();
+		return allResults;
+	}
 }
 
 module.exports = AdminRepository;
