dc7f81e07d8b3b74f1eddf97d377537b1400720c
alvarengacarlos <carlosho200@hotmail.com>
2022-04-25 00:15:25 -0400

    feat: Create ElectionResearch entity

--- contract/lib/Classes/ElectionResearch.js
+++ contract/lib/Classes/ElectionResearch.js
@@ -2,31 +2,57 @@ const ExistingRecord = require("../Exceptions/ExistingRecord");
 const Serializer = require("./Serializer");
 const CandidateDoesNotExist = require("../Exceptions/CandidateDoesNotExist");
 const Candidate = require("./Candidate");
+const ElectionResearchAlreadyStarted = require("../Exceptions/ElectionResearchAlreadyStarted");
+const ElectionResearchAlreadyClosed = require("../Exceptions/ElectionResearchAlreadyClosed");
 
 class ElectionResearch extends Serializer {
-    //TODO: Enquanto a pesquisa não for iniciada é possível adicionar e remover os candidatos.
-    // Depois que for inicada não deve ser mais possível. Nem mesmo depois de ser finalizada
+    
     constructor(id) {
         super();
         this.id = id;
         this.candidatesList = [];
-        this.start = null;
-        this.close = null;
+        this.start = false;
+        this.close = false;
         this.createIn = new Date().toString();
         this.finishIn = null;
     }
 
     static makeElectionResearch(year, month) {        
-        const id = `${year}-${month}`;
+        const id = `${year}-${month}`;        
         
         return new ElectionResearch(id);
+    }
+
+    static mountsObjectRetrievedFromTheBlockchain(electionResearchObject) {
+        const electionResearch = new ElectionResearch(null);
+        
+        electionResearch.id = electionResearchObject.id;
+        electionResearch.candidatesList = electionResearchObject.candidatesList;
+        electionResearch.start = electionResearchObject.start;
+        electionResearch.close = electionResearchObject.close;
+        electionResearch.createIn = electionResearchObject.createIn;
+        electionResearch.finishIn = electionResearchObject.finishIn;
+
+        return electionResearch;
+    }
+
+    startElectoralResearch() {
+        this.start = true;
     }
 
     getId() {
         return this.id;
     }
 
-    insertCandidates(candidate) {
+    insertCandidate(candidate) {
+        if (this.start) {
+            throw new ElectionResearchAlreadyStarted();
+        }
+
+        if (this.close) {
+            throw new ElectionResearchAlreadyClosed();
+        }
+
         for (const c of this.candidatesList) {                         
             if(c.getId() == candidate.getId()) {
                 throw new ExistingRecord();
