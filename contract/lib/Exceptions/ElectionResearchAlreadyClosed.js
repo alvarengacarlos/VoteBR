f31008e2c68f597ece43ec5d731a894c377adc2b
alvarengacarlos <carlosho200@hotmail.com>
2022-04-25 22:43:16 -0400

    feat: Create gateway for contract

--- contract/lib/Exceptions/ElectionResearchAlreadyClosed.js
+++
@@ -1,9 +0,0 @@
-const Exception = require("./Exception");
-
-class ElectionResearchAlreadyClosed extends Exception {
-    constructor() {
-		super("ELECTION_RESEARCH_ALREADY_CLOSED", 400, "Election research already closed");
-	} 
-}
-
-module.exports = ElectionResearchAlreadyClosed;
--- contract/lib/VoteBr.js
+++ contract/lib/VoteBr.js
@@ -3,6 +3,7 @@ const { Contract } = require("fabric-contract-api");
 const AdminValidation = require("./Validation/AdminValidation");
 const AdminService = require("./Service/AdminService");
 const AccessDenied = require("./Exceptions/AccessDenied");
+const AdminRepository = require("./Repository/AdminRepository");
 
 class VoteBr extends Contract {
     
@@ -12,8 +13,40 @@ class VoteBr extends Contract {
 		const adminValidation = new AdminValidation();
 		adminValidation.validateCreateElectionResearch(year, month);
 		
+		const adminRepository = new AdminRepository();
+
 		const adminService = new AdminService();
-		await adminService.createElectionResearch(ctx, year, month);
+		await adminService.createElectionResearch(ctx, adminRepository, year, month);
+	}
+
+	async insertCandidateInTheElectionResearch(ctx, name, numberOfCandidate) {
+		this._checkAuthorityAdmin(ctx);
+
+		const adminValidation = new AdminValidation();
+		adminValidation.validateInsertCandidateInTheElectionResearch(name, numberOfCandidate);
+
+		const adminRepository = new AdminRepository();
+
+		const adminService = new AdminService();
+		await adminService.insertCandidateInTheElectionResearch(ctx, adminRepository, name, numberOfCandidate);
+	}
+
+	async beginCollectingVotes(ctx) {
+		this._checkAuthorityAdmin(ctx);
+
+		const adminRepository = new AdminRepository();
+
+		const adminService = new AdminService();
+		await adminService.beginCollectingVotes(ctx, adminRepository);
+	}
+	
+	async finishCollectingVotesAndElectionResearch(ctx) {
+		this._checkAuthorityAdmin(ctx);
+
+		const adminRepository = new AdminRepository();
+
+		const adminService = new AdminService();
+		await adminService.finishCollectingVotesAndElectionResearch(ctx, adminRepository);
 	}
 
 	_checkAuthorityAdmin(ctx) {
