8c782174568cdbd6c3ecd70dd165dc9fd77d2b53
alvarengacarlos <carlosho200@hotmail.com>
2022-04-30 19:49:40 -0400

    feat: Create validateSearchElectionResearchLikeAdmin method to AdminValidation class

--- contract/lib/Validation/AdminValidation.js
+++ contract/lib/Validation/AdminValidation.js
@@ -33,6 +33,21 @@ class AdminValidation {
 		return value;		
 	}
 
+	validateSearchElectionResearchLikeAdmin(year, month) {
+		const schema = Joi.object({			
+			year: Joi.string().pattern(/^[0-9]+$/).min(4).max(4).required(),
+			month: Joi.string().pattern(/^0[1-9]|1[0-2]$/).min(2).max(2).required()			
+		});
+
+		const {error, value} = schema.validate({year, month});
+
+		if (error) {
+			throw new IncorrectInformationReceived();
+		}
+
+		return value;
+	}
+
 }
 
 module.exports = AdminValidation;
