0f9c3403099b3e76b00aaa1a6e2427ac5faffe3a
alvarengacarlos <carlosho200@hotmail.com>
2022-04-30 19:04:46 -0400

    delete: Remove unnecessary exception

--- contract/lib/Exceptions/ElectionResearchAlreadyStarted.js
+++
@@ -1,9 +0,0 @@
-const Exception = require("./Exception");
-
-class ElectionResearchAlreadyStarted extends Exception {
-    constructor() {
-		super("ELECTION_RESEARCH_ALREADY_STARTED", 400, "Election research already started");
-	} 
-}
-
-module.exports = ElectionResearchAlreadyStarted;
