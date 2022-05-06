003ce19a29390f7a4f023b6200360734dadd03fa
alvarengacarlos <carlosho200@hotmail.com>
2022-05-06 07:00:06 -0400

    feat: Create elector service

---
+++ application/src/App/Service/Elector.js
@@ -0,0 +1,111 @@
+const InvalidCpf = require("../Exception/Elector/InvalidCpf");
+const InvalidAge = require("../Exception/Elector/InvalidAge");
+const ApiSearchCpf = require("./ApiSearchCpf");
+const crypto = require("crypto");
+
+class Elector extends ApiSearchCpf {
+
+	async vote(payload) {		
+        const cpf = payload.cpf;
+        const birthDate = payload.birthDate;
+        const numberOfCandidate = payload.numberOfCandidate;
+
+		const birthDateObject = this.getBirthDateObject(birthDate);
+
+        const cpfIsValid = this.cpfIsValid(cpf);
+        if (!cpfIsValid) {
+            throw new InvalidCpf();
+        }
+
+        const ageIsValid = this.ageIsValid(birthDateObject);
+        if (!ageIsValid) {
+            throw new InvalidAge();
+        }
+		
+		await this.validatesIfElectorIsReal(cpf, birthDateObject);        
+		
+		const cpfHash = this.encryptCpf(cpf);
+		//TODO: SmartContract connection
+		return {cpf, numberOfCandidate};        
+    }
+
+	getBirthDateObject(birthDate) {
+		const dateArray = birthDate.split("-");
+		
+		const birthDateObject = {
+			year: dateArray[0],
+			month: dateArray[1],
+			day: dateArray[2] 
+		};		
+
+		return birthDateObject;
+	}
+
+    cpfIsValid(cpf) {
+		var sum;
+		var mod;
+		sum = 0;
+
+		if (cpf == "00000000000") {
+			return false;
+		}
+
+		for (let i = 1; i <= 9; i++) {
+			sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+		}
+
+		mod = (sum * 10) % 11;
+		if ((mod == 10) || (mod == 11)) {
+			mod = 0;
+		}
+
+		if (mod != parseInt(cpf.substring(9, 10))) {
+			return false;
+		}
+
+		sum = 0;
+		for (let i = 1; i <= 10; i++) {
+			sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+		}
+
+		mod = (sum * 10) % 11;
+		if ((mod == 10) || (mod == 11)) {
+			mod = 0;
+		}
+
+		if (mod != parseInt(cpf.substring(10, 11))) {
+			return false;
+		}
+
+		return true;
+	}
+
+    ageIsValid(birthDateObject) {
+        const MINIMUM_AGE = 16;
+
+		const year = new Date().getFullYear();
+		const month = new Date().getMonth();
+		const day = new Date().getDay();
+		
+		const toDay = new Date(`${year}-${month}-${day}`);
+		const birthDate = new Date(`${birthDateObject.year}-${birthDateObject.month}-${birthDateObject.day}`);
+
+		const age =  (toDay.getFullYear() - birthDate.getFullYear());		
+
+        if (age >= MINIMUM_AGE) {
+            return true
+        }
+
+        return false;
+	}	
+
+	encryptCpf(cpf) {
+		const hash = crypto.createHash("sha256");
+		const cpfHashing  = hash.update(cpf, "utf-8");
+		
+		return cpfHashing.digest("hex")
+	}
+
+}
+
+module.exports = Elector;
---
+++ application/test/Unit/Service/Elector.test.js
@@ -0,0 +1,151 @@
+const { describe, beforeEach, it } = require("mocha");
+const sinon = require("sinon");
+
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+chai.should();
+
+const ElectorService = require("../../../src/App/Service/Elector");
+const InvalidCpf = require("../../../src/App/Exception/Elector/InvalidCpf");
+const InvalidAge = require("../../../src/App/Exception/Elector/InvalidAge");
+
+describe("Elector", () => {
+    
+    let electorService, requestLibFake;
+
+    beforeEach(() => {
+        electorService = new ElectorService();
+        
+        class RequestLibFake {
+            get(){};
+        }
+        requestLibFake = sinon.createStubInstance(RequestLibFake);
+        electorService.requestLib = requestLibFake;
+    });
+
+    describe("#getBirthDateObject", () => {
+
+        it("Must return a birth date object", () => {
+            const birthDateString = "2000-02-01";
+
+            const birthDateObject = electorService.getBirthDateObject(birthDateString);
+
+            expect(birthDateObject).to.eql({ year: "2000", month: "02", day: "01" });
+        });
+
+    });
+
+    describe("#cpfIsValid", () => {
+
+        it("Must return true", () => {
+            const cpf = "01234567890";
+
+            const result = electorService.cpfIsValid(cpf);
+
+            expect(result).to.eql(true);
+        });
+
+        it("Must return false", () => {
+            const cpf = "01234567891";
+
+            const result = electorService.cpfIsValid(cpf);
+
+            expect(result).to.eql(false);
+        });
+
+    });
+
+    describe("#ageIsValid", () => {
+
+        it("Must return true", () => {
+            const birthDateString = "2000-02-01";
+            const birthDateObject = electorService.getBirthDateObject(birthDateString);
+
+            const result = electorService.ageIsValid(birthDateObject);
+
+            expect(result).to.eql(true);
+        });
+
+        it("Must return false", () => {
+            const year = new Date().getFullYear();
+            const month = new Date().getMonth();
+            const day = new Date().getDay();
+            const birthDateString = `${year}-${month}-${day}`;
+
+            const birthDateObject = electorService.getBirthDateObject(birthDateString);
+
+            const result = electorService.ageIsValid(birthDateObject);
+
+            expect(result).to.eql(false);
+        });
+
+    });
+
+    describe("#encryptCpf", () => {
+        
+        it("Must encrypt cpf", () => {
+            const cpf = "01234567890";
+            
+            const result = electorService.encryptCpf(cpf);
+            
+            console.log(result);
+            expect(result).to.eql("ee29eb4a8725678278ac439cf7abfd2a849cdc7378a6b6316017b81c51d720e7");
+        });
+
+    });
+
+    describe("#vote", () => {
+
+        it("Must throw invalid cpf", async () => {
+            const payload = {
+                cpf: "01234567891",
+                birthDate: "2000-06-23"
+            };
+            
+            await electorService.vote(payload).should.be.rejectedWith(InvalidCpf);
+        });
+
+        it("Must throw invalid age", async () => {
+            const year = new Date().getFullYear();
+            const month = new Date().getMonth();
+            const day = new Date().getDay();
+            const birthDateString = `${year}-${month}-${day}`;
+
+            const payload = {
+                cpf: "01234567890",
+                birthDate: birthDateString
+            };
+            
+            await electorService.vote(payload).should.be.rejectedWith(InvalidAge);
+        });
+
+        it("Must be successfull", async () => {
+            requestLibFake.get.callsFake(async () => {
+                const response = {
+                    status: 200,
+                    data: {code: 0}
+                };
+
+                return response;
+            });         
+            
+            const payload = {
+                cpf: "01234567890",
+                birthDate: "2000-06-23",
+                numberOfCandidate: "01"
+            };
+            
+            
+            const result = await electorService.vote(payload);
+            
+            expect(result).to.eql({
+                cpf: payload.cpf, 
+                numberOfCandidate: payload.numberOfCandidate
+            });
+        });
+
+    });
+
+});
