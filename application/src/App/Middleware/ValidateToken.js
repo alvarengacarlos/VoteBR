3c0692ee9caf07b46ecececc0ffa9f661ddf440d
alvarengacarlos <carlosho200@hotmail.com>
2022-05-07 21:19:23 -0400

    feat: Add validation elector token

--- application/src/App/Middleware/ValidateToken.js
+++ application/src/App/Middleware/ValidateToken.js
@@ -24,6 +24,27 @@ class ValidateToken {
 
         next();
     }
+
+    validateTokenForElector(req, res, next) {
+        const token = req.headers['token'];
+
+        if (!token) {
+            const exception = new TokenExpired();
+            const ef = ExceptionFormatter.formatApiException(exception);
+            res.status(ef.httpStatusCode).json(ef) 
+        }
+
+        const tokenProvider = new TokenProvider();
+        try {
+            tokenProvider.verifyTokenElector(token);
+        
+        } catch(exception) {                     
+            const ef = ExceptionFormatter.formatApiException(exception);
+            return res.status(ef.httpStatusCode).json(ef);
+        }
+
+        next();
+    }
 }
 
 module.exports = new ValidateToken();
