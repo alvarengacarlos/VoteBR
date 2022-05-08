c95e04cc6d8344473b6b36ebf05b81c9ba39de78
alvarengacarlos <carlosho200@hotmail.com>
2022-05-08 00:58:32 -0400

    feat: Create utils function for connection to smart contract

--- api-contract/src/Infra/Chaincode/AppUtil.js
+++ api-contract/src/Infra/Chaincode/AppUtil.js
@@ -1,38 +1,36 @@
-const { Wallets } = require('fabric-network')
-const pathLib = require('path')
-const fs = require('fs')
-const dotenv = require('dotenv').config({
-  path: `${pathLib.resolve(__dirname, '..', '..', '..')}/.env`,
-})
+const { Wallets } = require('fabric-network');
+const path = require('path');
+const fs = require('fs');
+const process = require('dotenv').config();
 
-const buildCCPOrg = (path = null) => {
-  if (path == null) {
-    path = dotenv.parsed.CCP_ABSOLUTE_PATH
-  }
+const CcpNotFound = require("../../App/Exception/Bootstrap/CcpNotFound");
 
-  const ccpPath = pathLib.resolve(path)
-  const fileExists = fs.existsSync(ccpPath)
+const buildCCPOrg = () => {  
+  const absolutePathCcp = process.parsed.CONTRACT_CCP_ABSOLUTE_PATH;  
+
+  const ccpPath = path.resolve(absolutePathCcp);
+  
+  const fileExists = fs.existsSync(ccpPath);
+  
+
   if (!fileExists) {
-    throw new Error(`no such file or directory: ${ccpPath}`)
-  }
-  const contents = fs.readFileSync(ccpPath, 'utf8')
-
-  const ccp = JSON.parse(contents)
-
-  console.log(`Loaded the network configuration located at ${ccpPath}`)
-
-  return ccp
-}
-
-const buildWallet = async (walletPath = null) => {
-  if (walletPath == null) {
-    walletPath = pathLib.join(__dirname, '..', '..', 'wallet')
+    throw new CcpNotFound();
   }
 
-  let wallet = await Wallets.newFileSystemWallet(walletPath)
+  const contents = fs.readFileSync(ccpPath, 'utf8');
 
-  console.log(`Built a file system wallet at ${walletPath}`)
+  const ccp = JSON.parse(contents);
 
-  return wallet
+  return ccp;
 }
+
+const buildWallet = async () => {
+  
+  const walletPath = path.join(__dirname, '..', '..', '..', 'wallet');  
+
+  const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+  return wallet;
+}
+
 module.exports = { buildCCPOrg, buildWallet }
